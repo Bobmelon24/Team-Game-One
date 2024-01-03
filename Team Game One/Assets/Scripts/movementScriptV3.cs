f6ad7d623a116edee6ae21b649acf8dbd6f0eef8
Bobmelon24 <141355556+Bobmelon24@users.noreply.github.com>
2024-01-03 13:10:57 -0800

    More movement stuff fixed

--- Team Game One/Assets/Scripts/movementScriptV3.cs
+++ Team Game One/Assets/Scripts/movementScriptV3.cs
@@ -13,7 +13,7 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); //Makes sure that the rigid body can be called later
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -28,25 +28,36 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 NoMovement = new Vector2(0f, 0f);
+        Vector2 NoMovement = new Vector2(0f, 0f);  //No idea what this is for...
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         if (moveHorizontal > 0)
         {
             {
-                rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+                rb2d.velocity = new Vector2(speed, rb2d.velocity.y); //Right Movement
 
             }
         }
         if (moveHorizontal < 0)
         {
-            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y); //Left Movement
         }
+
+        /*
         if (Input.GetKeyDown(KeyCode.W) && InAir == false)
         {
             rb2d.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
         }
+        */
+
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && InAir == false) 
+        {
+            rb2d.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse); //Jump Code
+        }
+    }
 }
         
